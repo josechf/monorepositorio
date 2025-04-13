37eb7cca6d64c8e97832042f979f10642231f7c5
jose chavez <54457816+josechf@users.noreply.github.com>
2025-04-13 16:46:29 -0500

    actions separados6

--- backpipsas/index.js
+++ backpipsas/index.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "compa llegaron las pipsas!!!!!!",
+        message: "Llegaron las pipsas!!!!!!",
         input: event,
       },
       null,

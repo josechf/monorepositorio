143d4d771fabc9c9ec09cc7d64df71ff5a02c841
jose chavez <54457816+josechf@users.noreply.github.com>
2025-04-14 06:48:10 -0500

    full acceso cloud

--- backpipsas/index.js
+++ backpipsas/index.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "llego la pizza",
+        message: "llegaron las pipsas",
         input: event,
       },
       null,
